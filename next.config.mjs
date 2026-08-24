7847ee599302788fbfdcae9c9150922c6a4e4d89
Arishusman <arishusm12an@gmail.com>
2026-08-24 12:59:32 +0500

    Fix GitHub Pages asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Stepaccademy',
-  assetPrefix: '/Stepaccademy/',
+  basePath: '/Al-Hammad-Accademy.github.io/',
+  assetPrefix: '/Al-Hammad-Accademy.github.io/',
   trailingSlash: true,
   images: {
     unoptimized: true,
